93bd8a81de4db7aa3a5560221b84f2a197ce01e8
mmontone <marianomontone@gmail.com>
2014-09-03 14:07:19 -0300

    More CORS headers

--- src/cors.lisp
+++ src/cors.lisp
@@ -1,43 +1,80 @@
 (in-package :rest-server)
 
-(defclass cors-resource (api-resource)
+(defparameter +cors-default-allow-headers+
+  (list "Cache-Control"
+	"Pragma"
+	"Origin"
+	"Authorization"
+	"Content-Type"
+	"X-Requested-With"))
+
+(defparameter +cors-default-allow-methods+ (list :get :put :post))
+
+(defclass cors-mixin ()
   ((cors-enabled :initarg :cors-enabled
 		 :accessor cors-enabled
 		 :initform t)
-   (cors-origin :initarg :cors-origin
-	   :accessor cors-origin
-	   :initform "*")))
+   (cors-allow-origin :initarg :cors-allow-origin
+		      :accessor cors-allow-origin
+		      :initform "*")
+   (cors-allow-headers :initarg :cors-allow-headers
+		       :accessor cors-allow-headers
+		       :initform +cors-default-allow-headers+)
+   (cors-allow-methods :initarg :cors-allow-methods
+		       :accessor cors-allow-methods
+		       :initform +cors-default-allow-methods+)))
+
+(defun format-cors-allow-headers (headers)
+  (format nil "~{~A~^, ~}" headers))
+
+(defun format-cors-allow-methods (methods)
+  (format nil "~{~A~^, ~}" (mapcar (alexandria:compose #'string-upcase
+						      #'string)
+				  methods)))
+
+(defun send-cors-headers (cors)
+  (when (cors-enabled cors)
+    (setf (hunchentoot:header-out "Access-Control-Allow-Origin")
+	  (cors-allow-origin cors))
+    (setf (hunchentoot:header-out "Access-Control-Allow-Headers")
+	  (format-cors-allow-headers (cors-allow-headers cors)))
+    (setf (hunchentoot:header-out "Access-Control-Allow-Methods")
+	  (format-cors-allow-methods (cors-allow-methods cors)))))			  
 
 (defmethod resource-execute-function-implementation
     :after
-    ((resource cors-resource)
+    ((resource cors-mixin)
      api-function-implementation
      request)
-  (when (cors-enabled resource)
-    (setf (hunchentoot:header-out "Access-Control-Allow-Origin")
-	  (cors-origin resource))))
+  (send-cors-headers resource))
 
-(defmethod process-api-resource-option ((option (eql :cors)) resource &key (enabled t) (origin "*"))
+(defmethod process-api-resource-option ((option (eql :cors)) resource
+					&key (enabled t)
+					  (allow-origin "*")
+					  (allow-headers +cors-default-allow-headers+)
+					  (allow-methods +cors-default-allow-methods+))
   (dynamic-mixins:ensure-mix resource 'cors-resource)
   (setf (cors-enabled resource) enabled)
-  (setf (cors-origin resource) origin))
+  (setf (cors-allow-origin resource) allow-origin)
+  (setf (cors-allow-headers resource) allow-headers)
+  (setf (cors-allow-methods resource) allow-methods))
 
-(defclass cors-api (api-definition)
-  ((cors-enabled :initarg :cors-enabled
-		 :accessor cors-enabled
-		 :initform t)
-   (cors-origin :initarg :cors-origin
-	   :accessor cors-origin
-	   :initform "*")))
+(defclass cors-api (cors-mixin)
+  ())
 
 (defmethod api-execute-function-implementation :after
     ((api cors-api) api-function-implementation
      resource request)
-  (when (cors-enabled api)
-    (setf (hunchentoot:header-out "Access-Control-Allow-Origin")
-	  (cors-origin api))))
+  (send-cors-headers api))
 
-(defmethod process-api-option ((option (eql :cors)) api &key (enabled t) (origin "*"))
+(defmethod process-api-option ((option (eql :cors)) api
+			       &key
+				 (enabled t)
+				 (allow-origin "*")
+				 (allow-headers +cors-default-allow-headers+)
+				 (allow-methods +cors-default-allow-methods+))
   (dynamic-mixins:ensure-mix api 'cors-api)
   (setf (cors-enabled api) enabled)
-  (setf (cors-origin api) origin))
+  (setf (cors-allow-origin api) allow-origin)
+  (setf (cors-allow-headers api) allow-headers)
+  (setf (cors-allow-methods api) allow-methods))
