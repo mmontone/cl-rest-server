213a84f6b151ecea8ea70bc94b014d9b22895fd0
Mariano Montone <marianomontone@gmail.com>
2016-02-08 18:36:28 -0300

    Small refactor to swagger

--- src/api-documentation.lisp
+++ src/api-documentation.lisp
@@ -22,7 +22,7 @@
   (hunchentoot:start
    (make-instance 'api-documentation-acceptor
                   :address address
-		  :port port
+                  :port port
                   :api (if (symbolp api)
                            (find-api api)
                            api))))
@@ -42,36 +42,36 @@
             (cl-who:str (api-documentation api)))
       (:div :class "resource-operations"
             (loop for resource-operation being the hash-value of (resource-operations api)
-                 do
+               do
                  (document-resource-operation acceptor resource-operation))))))
-      
+
 (defun document-resource-operation (acceptor resource-operation)
   (cl-who:with-html-output (*standard-output*)
     (cl-who:htm
-   (:div :class "resource-operation"
-         (:div :class "name"
-               (:a :href (format nil "http://~A:~A/~A"
-                                 (hunchentoot:acceptor-address acceptor)
-                                 (hunchentoot:acceptor-port acceptor)
-                                 (name resource-operation))
-                   (cl-who:str (name resource-operation))))
-         (:div :class "method"
-               (cl-who:str (request-method resource-operation)))
-         (:div :class "documentation"
-               (cl-who:str (api-documentation resource-operation)))
-         (:div :class "signature"
-               (cl-who:str (path resource-operation)))
-         (:div :class "arguments"
-               (:ul
-                (loop for arg in (required-arguments resource-operation)
-                   do
-                   (cl-who:htm
-                    (:li (cl-who:fmt "~a : ~a. ~a." (first arg) (second arg) (third arg)))))
-                (loop for arg in (optional-arguments resource-operation)
-                   do
-                   (cl-who:htm
-                    (:li (cl-who:fmt "~a : ~a. ~A. Default: ~a"
-                                     (first arg)
-                                     (second arg)
-                                     (third arg)
-                                     (nth 3 arg)))))))))))
+     (:div :class "resource-operation"
+           (:div :class "name"
+                 (:a :href (format nil "http://~A:~A/~A"
+                                   (hunchentoot:acceptor-address acceptor)
+                                   (hunchentoot:acceptor-port acceptor)
+                                   (name resource-operation))
+                     (cl-who:str (name resource-operation))))
+           (:div :class "method"
+                 (cl-who:str (request-method resource-operation)))
+           (:div :class "documentation"
+                 (cl-who:str (api-documentation resource-operation)))
+           (:div :class "signature"
+                 (cl-who:str (path resource-operation)))
+           (:div :class "arguments"
+                 (:ul
+                  (loop for arg in (required-arguments resource-operation)
+                     do
+                       (cl-who:htm
+                        (:li (cl-who:fmt "~a : ~a. ~a." (first arg) (second arg) (third arg)))))
+                  (loop for arg in (optional-arguments resource-operation)
+                     do
+                       (cl-who:htm
+                        (:li (cl-who:fmt "~a : ~a. ~A. Default: ~a"
+                                         (first arg)
+                                         (second arg)
+                                         (third arg)
+                                         (nth 3 arg)))))))))))
--- src/swagger.lisp
+++ src/swagger.lisp
@@ -5,96 +5,100 @@
     `(progn
        ;; Swagger docs API definition
        (with-api ,api-name
-	 (define-api-resource api-docs (:documentation "Swagger API docs"
-						       :path "/api-docs")
-	   (api-docs-index (:request-method :get
-					    :path "/api-docs"
-					    :produces (:json))
-			   ())
-	   ,@(loop for resource in (list-api-resources api)
-		collect
-		  (let ((fname (intern (format nil "API-DOCS-~A" (string-upcase (resource-name resource))))))
-		    `(,fname (:request-method :get
-					      :path ,(format nil "/api-docs~A" (resource-path resource))
-					      :produces (:json))
-			     ()))))
+         (define-api-resource api-docs (:documentation "Swagger API docs"
+                                                       :path "/api-docs")
+           (api-docs-index (:request-method :get
+                                            :path "/api-docs"
+                                            :produces (:json))
+                           ())
+           ,@(loop for resource in (list-api-resources api)
+                collect
+                  (let ((fname (intern (format nil "API-DOCS-~A" (string-upcase (resource-name resource))))))
+                    `(,fname (:request-method :get
+                                              :path ,(format nil "/api-docs~A" (resource-path resource))
+                                              :produces (:json))
+                             ()))))
 
-	 ;; Swagger docs API  implementation
-	 (implement-resource-operation ,api-name api-docs-index ()
-	   (swagger-api-spec (find-api ',api-name)))
+         ;; Swagger docs API  implementation
+         (implement-resource-operation ,api-name api-docs-index ()
+           (swagger-api-spec (find-api ',api-name)))
 
-	 ;; Configure the API to work with Swagger
-	 (configure-api ',api-name
-			'(:swagger)
-			'(:cors :enabled t))
-     
-	 ,@(loop for resource in (list-api-resources api)
-	      collect
-		(let ((fname (intern (format nil "API-DOCS-~A" (string-upcase (resource-name resource))))))
-		  `(implement-resource-operation ,api-name ,fname ()
-		     (let ((api (find-api ',api-name)))
-		       (swagger-resource-spec api
-					      (find-api-resource ',(resource-name resource) :api api)
-					      (format nil "http://~A:~A"
-						      (hunchentoot:acceptor-address hunchentoot:*acceptor*)
-						      (hunchentoot:acceptor-port hunchentoot:*acceptor*)))))))))))
-  
-(defun swagger-api-spec (api)
+         ;; Configure the API to work with Swagger
+         (configure-api ',api-name
+                        '(:swagger)
+                        '(:cors :enabled t))
+
+         ,@(loop for resource in (list-api-resources api)
+              collect
+                (let ((fname (intern (format nil "API-DOCS-~A" (string-upcase (resource-name resource))))))
+                  `(implement-resource-operation ,api-name ,fname ()
+                     (let ((api (find-api ',api-name)))
+                       (swagger-resource-spec api
+                                              (find-api-resource ',(resource-name resource) :api api)
+                                              (format nil "http://~A:~A"
+                                                      (hunchentoot:acceptor-address hunchentoot:*acceptor*)
+                                                      (hunchentoot:acceptor-port hunchentoot:*acceptor*)))))))))))
+
+(defun swagger-api-docs-index (api)
   (setf (hunchentoot:header-out "Content-Type") "application/json")
   (setf (hunchentoot:header-out "Access-Control-Allow-Origin") "*")
   (with-output-to-string (json:*json-output*)
+    (render-swagger-api-spec api json:*json-output*)))
+  
+(defun render-swagger-api-spec (api stream)
+  (let ((json:*json-output* stream))
     (json:with-object ()
       (json:encode-object-member :api-version (or (version api) "0.1"))
       (json:encode-object-member :swagger-version "1.2")
       (json:as-object-member (:apis)
-	(json:with-array ()
-	  (loop for resource in (list-api-resources api)
-	       when (not (equalp (resource-name resource) 'api-docs))
-	     do
-	       (json:as-array-member ()
-		 (json:with-object ()
-		   (json:encode-object-member :path (resource-path resource))
-		   (json:encode-object-member :description (resource-documentation resource)))))))
+        (json:with-array ()
+          (loop for resource in (list-api-resources api)
+             when (not (equalp (resource-name resource) 'api-docs))
+             do
+               (json:as-array-member ()
+                 (json:with-object ()
+                   (json:encode-object-member :path (resource-path resource))
+                   (json:encode-object-member :description (resource-documentation resource)))))))
       (json:as-object-member (:info)
-	(json:with-object ()
-	  (json:encode-object-member :title (or (title api)
-						(name api)))
-	  (json:encode-object-member :description (api-documentation api)))))))
+        (json:with-object ()
+          (json:encode-object-member :title (or (title api)
+                                                (name api)))
+          (json:encode-object-member :description (api-documentation api)))))))
 
 (defun resource-apis (resource)
   "Get Swagger apis from the resource"
   (group-by:group-by (rest-server::list-api-resource-functions resource)
-		     :key #'rest-server::path
-		     :test #'equalp
-		     :value #'identity))
+                     :key #'rest-server::path
+                     :test #'equalp
+                     :value #'identity))
 
 (defun encode-swagger-operation (operation)
   (flet ((encode-parameter (parameter required-p)
-	   (if (not required-p)
-	       (with-accessors ((name argument-name)
-				(type argument-type)
-				(default-value argument-default)
-				(documentation argument-documentation))
-		   parameter
-		 (declare (ignore default-value))
-		 (json:with-object ()
-		   (json:encode-object-member :name name)
-		   (json:encode-object-member :type type)
-		   (json:encode-object-member :param-type "query")
-		   (json:encode-object-member :description documentation)
-		   (json:encode-object-member :required :false)
-		   (json:encode-object-member :allow-multiple :false)))
-	       (with-accessors ((name argument-name)
-				(type argument-type)
-				(documentation argument-documentation))
-		   parameter
-		 (json:with-object ()
-		   (json:encode-object-member :name name)
-		   (json:encode-object-member :type type)
-		   (json:encode-object-member :param-type "path")
-		   (json:encode-object-member :description documentation)
-		   (json:encode-object-member :required t)
-		   (json:encode-object-member :allow-multiple :false))))))
+           (if (not required-p)
+               (with-accessors ((name argument-name)
+                                (type argument-type)
+                                (default-value argument-default)
+                                (documentation argument-documentation))
+                   parameter
+                 (declare (ignore default-value))
+                 (json:with-object ()
+                   (json:encode-object-member :name name)
+                   (json:encode-object-member :type type)
+                   (json:encode-object-member :param-type "query")
+                   (json:encode-object-member :description documentation)
+                   (json:encode-object-member :required :false)
+                   (json:encode-object-member :allow-multiple :false)))
+               (with-accessors ((name argument-name)
+                                (type argument-type)
+                                (documentation argument-documentation))
+                   parameter
+                 (json:with-object ()
+                   (json:encode-object-member :name name)
+                   (json:encode-object-member :type type)
+                   (json:encode-object-member :param-type "path")
+                   (json:encode-object-member :description documentation)
+                   (json:encode-object-member :required t)
+                   (json:encode-object-member :allow-multiple :false))))))
     (json:with-object ()
       (json:encode-object-member :method (string-upcase (symbol-name (request-method operation))))
       (json:encode-object-member :summary (api-summary operation))
@@ -102,32 +106,32 @@
       (json:encode-object-member :nickname (symbol-name (name operation)))
       #+nil(json:encode-object-member :type (resource-operation-type operation))
       (when (produces operation)
-	(json:encode-object-member :produces (mapcar #'mime-to-string (produces operation))))
+        (json:encode-object-member :produces (mapcar #'mime-to-string (produces operation))))
       (when (consumes operation)
-	(json:encode-object-member :consumes (mapcar #'mime-to-string (consumes operation))))      
+        (json:encode-object-member :consumes (mapcar #'mime-to-string (consumes operation))))
       (json:as-object-member (:parameters)
-	(json:with-array ()
-	  (loop for parameter in (required-arguments operation)
-	     do
-	       (json:as-array-member ()
-		 (encode-parameter parameter t)))
-	  (loop for parameter in (optional-arguments operation)
-	     do
-	       (json:as-array-member ()
-		 (encode-parameter parameter nil)))
-	  (when (member (request-method operation) (list :post :put :patch))
-	    (json:as-array-member ()
-	      (json:with-object ()
-		(json:encode-object-member :name "body")
-		(json:encode-object-member :type (string-downcase (princ-to-string (body-type operation))))
-		(json:encode-object-member :param-type "body")
-		(json:encode-object-member :description "The content")
-		(json:encode-object-member :required t)
-		(json:encode-object-member :allow-multiple :false))))))
+        (json:with-array ()
+          (loop for parameter in (required-arguments operation)
+             do
+               (json:as-array-member ()
+                 (encode-parameter parameter t)))
+          (loop for parameter in (optional-arguments operation)
+             do
+               (json:as-array-member ()
+                 (encode-parameter parameter nil)))
+          (when (member (request-method operation) (list :post :put :patch))
+            (json:as-array-member ()
+              (json:with-object ()
+                (json:encode-object-member :name "body")
+                (json:encode-object-member :type (string-downcase (princ-to-string (body-type operation))))
+                (json:encode-object-member :param-type "body")
+                (json:encode-object-member :description "The content")
+                (json:encode-object-member :required t)
+                (json:encode-object-member :allow-multiple :false))))))
       (json:as-object-member (:authorizations)
-	(encode-swagger-authorizations (authorizations operation)))
+        (encode-swagger-authorizations (authorizations operation)))
       )))
-      
+
 (defun swagger-resource-spec (api resource base-url)
   (setf (hunchentoot:header-out "Content-Type") "application/json")
   (setf (hunchentoot:header-out "Access-Control-Allow-Origin") "*")
@@ -144,21 +148,21 @@
       (json:encode-object-member
        :produces (mapcar #'mime-to-string (resource-produces resource)))
       (json:as-object-member (:apis)
-	(json:with-array ()
-	  (loop for api in (resource-apis resource)
-	     do
-	       (json:as-array-member ()
-		 (destructuring-bind (path &rest operations) api
-		   (json:with-object ()
-		     (json:encode-object-member :path path)
-		     (json:as-object-member (:operations)
-		       (json:with-array ()
-			 (loop for operation in operations
-			    do (json:as-array-member ()
-				 (encode-swagger-operation operation)))))))))))      
+        (json:with-array ()
+          (loop for api in (resource-apis resource)
+             do
+               (json:as-array-member ()
+                 (destructuring-bind (path &rest operations) api
+                   (json:with-object ()
+                     (json:encode-object-member :path path)
+                     (json:as-object-member (:operations)
+                       (json:with-array ()
+                         (loop for operation in operations
+                            do (json:as-array-member ()
+                                 (encode-swagger-operation operation)))))))))))
       (json:as-object-member (:models)
-	(encode-swagger-models (mapcar #'rs.schema:find-schema (resource-models resource))))
-	)))
+        (encode-swagger-models (mapcar #'rs.schema:find-schema (resource-models resource))))
+      )))
 
 (defun mime-to-string (mime-type)
   (cond
@@ -175,62 +179,62 @@
 
 (defun encode-swagger-model (schema-element)
   (flet ((encode-model-property (attribute)
-	   (json:as-object-member ((rs.schema::attribute-name attribute))
-	     (json:with-object ()
-	       (let ((attribute-type (rs.schema::attribute-type attribute)))
-		 (cond
-		   ((keywordp attribute-type) ;; A primitive type
-		    (json:encode-object-member
-		     :type
-		     (string-downcase (princ-to-string attribute-type))))
-		   ((symbolp attribute-type) ;; A schema reference
-		    (json:encode-object-member
-		     "$ref" (string-downcase (princ-to-string attribute-type))))
-		   ((listp attribute-type) ;; A composite type
-		    (ecase (first attribute-type)
-		      (:list (json:encode-object-member
-			      :type "array")
-			     (json:as-object-member (:items)
-			       (json:with-object ()
-				 (cond
-				   ((keywordp (second attribute-type))
-				    (json:encode-object-member :type
-							       (string-downcase
-								(princ-to-string (second attribute-type)))))
-				   ((symbolp (second attribute-type))
-				    (json:encode-object-member "$ref"
-							       (string-downcase (princ-to-string (second attribute-type)))))
-				   (t (error "Don't know how to encode this"))))))
-		      (:option (json:encode-object-member :enum
-							  (rest attribute-type)))
-		      (:element (error "Not implemented yet"))))))		     
-	       (let ((desc (or (rs.schema::attribute-option :description attribute)
-			       (rs.schema::attribute-option :documentation attribute))))
-		 (when desc
-		   (json:encode-object-member :description desc)))))))
+           (json:as-object-member ((rs.schema::attribute-name attribute))
+             (json:with-object ()
+               (let ((attribute-type (rs.schema::attribute-type attribute)))
+                 (cond
+                   ((keywordp attribute-type) ;; A primitive type
+                    (json:encode-object-member
+                     :type
+                     (string-downcase (princ-to-string attribute-type))))
+                   ((symbolp attribute-type) ;; A schema reference
+                    (json:encode-object-member
+                     "$ref" (string-downcase (princ-to-string attribute-type))))
+                   ((listp attribute-type) ;; A composite type
+                    (ecase (first attribute-type)
+                      (:list (json:encode-object-member
+                              :type "array")
+                             (json:as-object-member (:items)
+                               (json:with-object ()
+                                 (cond
+                                   ((keywordp (second attribute-type))
+                                    (json:encode-object-member :type
+                                                               (string-downcase
+                                                                (princ-to-string (second attribute-type)))))
+                                   ((symbolp (second attribute-type))
+                                    (json:encode-object-member "$ref"
+                                                               (string-downcase (princ-to-string (second attribute-type)))))
+                                   (t (error "Don't know how to encode this"))))))
+                      (:option (json:encode-object-member :enum
+                                                          (rest attribute-type)))
+                      (:element (error "Not implemented yet"))))))
+               (let ((desc (or (rs.schema::attribute-option :description attribute)
+                               (rs.schema::attribute-option :documentation attribute))))
+                 (when desc
+                   (json:encode-object-member :description desc)))))))
     (json:with-object ()
       (json:encode-object-member :id (rs.schema::element-name schema-element))
       (json:as-object-member (:properties)
-	(json:with-object ()
-	  (loop for attribute in (rs.schema::element-attributes schema-element)
-	     do
-	       (encode-model-property attribute)))))))
+        (json:with-object ()
+          (loop for attribute in (rs.schema::element-attributes schema-element)
+             do
+               (encode-model-property attribute)))))))
 
 (defun encode-swagger-models (schemas)
   (json:with-object ()
     (loop for schema in schemas
-	 do
-	 (json:as-object-member ((rs.schema::element-name schema))
-	   (encode-swagger-model schema)))))
+       do
+         (json:as-object-member ((rs.schema::element-name schema))
+           (encode-swagger-model schema)))))
 
 (defun encode-swagger-authorization (authorization)
   (json:with-object ()
     (json:encode-object-member :type (cond
-					 ((subtypep authorization 'token-authentication)
-					  "apiKey")
-					 ((subtypep authorization 'oauth2-authentication)
-					  "oauth2")
-					 (t (error "Not implemented"))))
+                                       ((subtypep authorization 'token-authentication)
+                                        "apiKey")
+                                       ((subtypep authorization 'oauth2-authentication)
+                                        "oauth2")
+                                       (t (error "Not implemented"))))
     (json:encode-object-member :pass-as "header")
     (when (subtypep authorization 'token-authentication)
       (json:encode-object-member :keyname ""))))
@@ -238,8 +242,8 @@
 (defun encode-swagger-authorizations (authorizations)
   (json:with-object ()
     (loop for authorization in authorizations
-	 do (json:as-object-member ((class-name (class-of authorization)))
-	      (encode-swagger-authorization authorization)))))
+       do (json:as-object-member ((class-name (class-of authorization)))
+            (encode-swagger-authorization authorization)))))
 
 (defclass swagger-resource (api-resource)
   ())
