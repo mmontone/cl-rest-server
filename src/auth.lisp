67a85a33fda59ef67c07b86e0dc06920e9baa234
Mariano Montone <marianomontone@gmail.com>
2018-04-11 00:34:27 -0300

    Move crypto to util

--- src/auth.lisp
+++ src/auth.lisp
@@ -3,101 +3,11 @@
 (defvar *authorization-enabled-p* t "Globally enable/disable API authorization")
 (defvar *auth* nil "The succesful authorization. Bound for resource-operations (the resource operation can tell how it was authorized)")
 
-(defvar *key* nil)
-
-(defun register-key (key)
-  (setf *key*
-        (let* ((octets (babel:string-to-octets key))
-               (length (length octets)))
-          (and (> length 0)
-               (flet ((pad-to (n)
-                        "Is that a good idea, or should I just 0-pad?"
-                        (apply 'concatenate
-                               '(vector (unsigned-byte 8))
-                               (subseq octets 0 (rem n length))
-                               (make-list (floor n length)
-                                          :initial-element octets))))
-                 (ironclad:make-cipher :aes
-                                       :key  (cond ((<= length 16) (pad-to 16))
-                                                   ((<= length 24) (pad-to 23))
-                                                   ((<= length 32) (pad-to 32))
-                                                   (t (error "Maximum key size for AES: 32 bytes")))
-                                       :mode :ecb))))))
-
-(defun compress (string)
-  (babel:string-to-octets string))
-
-(defun inflate (ub8s)
-  (babel:octets-to-string ub8s))
-
-(defun word-to-octets (x)
-  (declare (type (unsigned-byte 32) x))
-  (make-array 4
-              :element-type '(unsigned-byte 8)
-              :initial-contents (list (ldb (byte 8 0) x)
-                                      (ldb (byte 8 8) x)
-                                      (ldb (byte 8 16) x)
-                                      (ldb (byte 8 24) x))))
-
-(defun octets-to-word (octets)
-  (+ (* (expt 2 0)  (aref octets 0))
-     (* (expt 2 8)  (aref octets 1))
-     (* (expt 2 16) (aref octets 2))
-     (* (expt 2 24) (aref octets 3))))
-
-(defun encrypt (octets)
-  (if (null *key*)
-      octets
-      (let* ((octets (concatenate '(vector (unsigned-byte 8))
-                                  (word-to-octets (length octets))
-                                  octets))
-             (octets (concatenate '(vector (unsigned-byte 8))
-                                  octets
-                                  (make-array (- (* 16 (ceiling (length octets) 16))
-                                                 (length octets))
-                                              :element-type '(unsigned-byte 8)
-                                              :initial-element 0)))
-             (out    (make-array (* 2 (length octets))
-                                 :element-type '(unsigned-byte 8)))
-             (length (nth-value 1
-                                (ironclad:encrypt *key*
-                                                  octets
-                                                  out))))
-        (subseq out 0 length))))
-
-(defun decrypt (octets)
-  (if (null *key*)
-      octets
-      (let* ((out (make-array (* 2 (length octets))
-                              :element-type '(unsigned-byte 8)
-                              :initial-element 0))
-             (length (nth-value 1
-                                (ironclad:decrypt *key* octets out)))
-             (out    (subseq out 0 length)))
-        (subseq out 4 (+ 4 (octets-to-word out))))))
-
-(defun encode-string (str)
-  (cl-base64:usb8-array-to-base64-string
-   (encrypt
-    (compress
-     (prin1-to-string str)))
-   :uri t))
-
-(defun decode-string (b64-str)
-  (read-from-string
-   (inflate
-    (decrypt
-     (cl-base64:base64-string-to-usb8-array b64-str
-                                            :uri t)))
-   nil))
-
-(register-key "zgpaZoegGAbg3G480jn340fnsS3ia")
-
 (defun user-authorization-token (id username)
-  (encode-string (format nil "(~A ~A)" id username)))
+  (rs::encode-string (format nil "(~A ~A)" id username)))
 
 (defun decode-token (token)
-  (ignore-errors (read-from-string (decode-string token))))
+  (ignore-errors (read-from-string (rs::decode-string token))))
 
 #+nil(defun authorization-token-user (token)
        (let ((username-and-pass (decode-token token)))
--- src/util.lisp
+++ src/util.lisp
@@ -78,3 +78,97 @@ Returns a lambda list with the special arguments removed, and the removed specia
 
 (defmacro with-lisp-json (&body body)
   `(call-with-lisp-json (lambda () ,@body)))
+
+;;--------------
+;; Basic crypto
+;;--------------
+
+(defvar *key* nil)
+
+(defun register-key (key)
+  (setf *key*
+        (let* ((octets (babel:string-to-octets key))
+               (length (length octets)))
+          (and (> length 0)
+               (flet ((pad-to (n)
+                        "Is that a good idea, or should I just 0-pad?"
+                        (apply 'concatenate
+                               '(vector (unsigned-byte 8))
+                               (subseq octets 0 (rem n length))
+                               (make-list (floor n length)
+                                          :initial-element octets))))
+                 (ironclad:make-cipher :aes
+                                       :key  (cond ((<= length 16) (pad-to 16))
+                                                   ((<= length 24) (pad-to 23))
+                                                   ((<= length 32) (pad-to 32))
+                                                   (t (error "Maximum key size for AES: 32 bytes")))
+                                       :mode :ecb))))))
+
+(defun compress (string)
+  (babel:string-to-octets string))
+
+(defun inflate (ub8s)
+  (babel:octets-to-string ub8s))
+
+(defun word-to-octets (x)
+  (declare (type (unsigned-byte 32) x))
+  (make-array 4
+              :element-type '(unsigned-byte 8)
+              :initial-contents (list (ldb (byte 8 0) x)
+                                      (ldb (byte 8 8) x)
+                                      (ldb (byte 8 16) x)
+                                      (ldb (byte 8 24) x))))
+
+(defun octets-to-word (octets)
+  (+ (* (expt 2 0)  (aref octets 0))
+     (* (expt 2 8)  (aref octets 1))
+     (* (expt 2 16) (aref octets 2))
+     (* (expt 2 24) (aref octets 3))))
+
+(defun encrypt (octets)
+  (if (null *key*)
+      octets
+      (let* ((octets (concatenate '(vector (unsigned-byte 8))
+                                  (word-to-octets (length octets))
+                                  octets))
+             (octets (concatenate '(vector (unsigned-byte 8))
+                                  octets
+                                  (make-array (- (* 16 (ceiling (length octets) 16))
+                                                 (length octets))
+                                              :element-type '(unsigned-byte 8)
+                                              :initial-element 0)))
+             (out    (make-array (* 2 (length octets))
+                                 :element-type '(unsigned-byte 8)))
+             (length (nth-value 1
+                                (ironclad:encrypt *key*
+                                                  octets
+                                                  out))))
+        (subseq out 0 length))))
+
+(defun decrypt (octets)
+  (if (null *key*)
+      octets
+      (let* ((out (make-array (* 2 (length octets))
+                              :element-type '(unsigned-byte 8)
+                              :initial-element 0))
+             (length (nth-value 1
+                                (ironclad:decrypt *key* octets out)))
+             (out    (subseq out 0 length)))
+        (subseq out 4 (+ 4 (octets-to-word out))))))
+
+(defun encode-string (str)
+  (cl-base64:usb8-array-to-base64-string
+   (encrypt
+    (compress
+     (prin1-to-string str)))
+   :uri t))
+
+(defun decode-string (b64-str)
+  (read-from-string
+   (inflate
+    (decrypt
+     (cl-base64:base64-string-to-usb8-array b64-str
+                                            :uri t)))
+   nil))
+
+(register-key "zgpaZoegGAbg3G480jn340fnsS3ia")
