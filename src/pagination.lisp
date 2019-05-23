e06f9baae1a349c06241d41f4d7df05dde0cc265
Mariano Montone <marianomontone@gmail.com>
2019-05-23 13:55:57 -0300

    Indentation

--- src/pagination.lisp
+++ src/pagination.lisp
@@ -1,10 +1,10 @@
 (in-package :rest-server)
 
 (defmacro with-pagination ((&rest args
-				  &key (page (error "Provide the page"))
-				  (object-name "pagination")
-				  &allow-other-keys)
-			   &body body)
+                                  &key (page (error "Provide the page"))
+                                  (object-name "pagination")
+                                  &allow-other-keys)
+                           &body body)
   (let* ((fargs (copy-list args)))
     (remf fargs :page)
     (remf fargs :object-name)
@@ -13,60 +13,60 @@
        (rs.serialize:set-attribute :next (format-absolute-resource-operation-url *resource-operation* :page (1+ ,page) ,@fargs))
        (rs.serialize:set-attribute :previous (format-absolute-resource-operation-url *resource-operation* :page (1- ,page),@fargs))
        (rs.serialize:with-attribute (:results)
-	 ,@body))))
+         ,@body))))
 
 (defun make-pagination-object (&rest args
-				&key (page (error "Provide the page"))
-				  (object-name "pagination")
-				  (results (error "Provide the results"))
-				  &allow-other-keys)
+                               &key (page (error "Provide the page"))
+                                 (object-name "pagination")
+                                 (results (error "Provide the results"))
+                                 &allow-other-keys)
   (let* ((fargs (copy-list args)))
     (remf fargs :page)
     (remf fargs :object-name)
     (remf fargs :results)
     (rs.serialize:object object-name
-	     (rs.serialize:attribute :page page)
-	     (rs.serialize:attribute :next
-			(format-absolute-resource-operation-url *resource-operation*
-							  `(:page (1+ ,page)
-								  ,@fargs)))
-	     (rs.serialize:attribute :previous
-			(format-absolute-resource-operation-url *resource-operation*
-							  `(:page (1- ,page)
-								  ,@fargs)))
-	     (rs.serialize:attribute :results results))))	     
+                         (rs.serialize:attribute :page page)
+                         (rs.serialize:attribute :next
+                                                 (format-absolute-resource-operation-url *resource-operation*
+                                                                                         `(:page (1+ ,page)
+                                                                                                 ,@fargs)))
+                         (rs.serialize:attribute :previous
+                                                 (format-absolute-resource-operation-url *resource-operation*
+                                                                                         `(:page (1- ,page)
+                                                                                                 ,@fargs)))
+                         (rs.serialize:attribute :results results))))
 
 ;; (defclass pagination ()
 ;;   ((function :initarg :function
-;; 	     :initform (error "Provide the function")
-;; 	     :accessor pagination-function)
+;;       :initform (error "Provide the function")
+;;       :accessor pagination-function)
 ;;    (page :initarg :page
-;; 	 :initform (error "Provide the page number")
-;; 	 :accessor pagination-page)
+;;   :initform (error "Provide the page number")
+;;   :accessor pagination-page)
 ;;    (page-size :initarg :page-size
-;; 	      :initform (error "Provide the page size")
-;; 	      :accessor pagination-page-size)))
+;;        :initform (error "Provide the page size")
+;;        :accessor pagination-page-size)))
 
 ;; (defun apply-pagination (pagination)
 ;;   (apply (pagination-function pagination)
-;; 	 (pagination-offset pagination)
-;; 	 (pagination-segment pagination)))	     
+;;   (pagination-offset pagination)
+;;   (pagination-segment pagination)))
 
 ;; (defmethod encode-pagination (pagination resource-operation (serializer (eql :json)) stream &rest args)
 ;;   (let ((objects (apply-pagination pagination)))
 ;;     (json:with-object (stream)
 ;;       (json:as-object-member (:objects stream)
-;; 	(loop for object in objects
-;; 	   do
-;; 	     (apply #'serialize object serializer stream args)))
+;;  (loop for object in objects
+;;     do
+;;       (apply #'serialize object serializer stream args)))
 ;;       (json:encode-object-member :page (pagination-page pagination) stream)
 ;;       (json:encode-object-member :next
-;; 				 (rest-server::format-resource-operation-url
-;; 				  resource-operation
-;; 				  :page (1+ (pagination-page pagination))
-;; 				  args))
+;;               (rest-server::format-resource-operation-url
+;;                resource-operation
+;;                :page (1+ (pagination-page pagination))
+;;                args))
 ;;       (json:encode-object-member :previous
-;; 				 (rest-server::format-resource-operation-url
-;; 				  resource-operation
-;; 				  :page (1- (pagination-page pagination))
-;; 				  args)))))
+;;               (rest-server::format-resource-operation-url
+;;                resource-operation
+;;                :page (1- (pagination-page pagination))
+;;                args)))))
